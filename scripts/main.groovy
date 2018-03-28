ba61d2d71d3af819a4d6f52d6f6e5d769798ea78
Dermot Burke <dermotmburke@gmail.com>
2018-03-28 16:49:19 +0100

    Add integration tests and error handling

--- scripts/main.groovy
+++ scripts/main.groovy
@@ -3,7 +3,21 @@ import com.buildit.artifactfetcher.ArtifactFetcher
 
 def jenkinsConfig = new ConfigFetcher().fetch()
 
-def version = jenkinsConfig.jenkins.version
-def artifactArray = new ArtifactFetcher().fetch(version.artifactPattern, [version.artifact])
+def version = jenkinsConfig?.jenkins?.version
 
-println(artifactArray[0])
+def artifactPattern = version?.artifactPattern
+def artifact = version?.artifact
+
+if(!artifactPattern || !artifact){
+    throw new IllegalArgumentException("Unable to download jenkins file. Config version dscovered was ${version}")
+}
+
+def artifactArray = new ArtifactFetcher().fetch(artifactPattern, [artifact])
+File warFile = artifactArray[0]
+
+if(!warFile || !warFile.exists()){
+    throw new IllegalArgumentException("Unable to download jenkins file. Config version dscovered was ${version}")
+}
+
+println(warFile.absolutePath)
+return warFile.absolutePath
